36d224368a02baa1bb0072586a0e830dce0295d9
olivers <olivers@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-02-25 16:32:25 +0000

    git-svn-id: https://plugins.svn.wordpress.org/cforms/tags/7.52@33020 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ contactforms/js/include/selectbox.php
@@ -0,0 +1,20 @@
+<?php require_once('../../../../../wp-blog-header.php'); ?>
+
+<form method="post"/>
+
+	<label for="cf_edit_label_select"><?php _e('Field label', 'cforms'); ?></label>
+	<input type="text" id="cf_edit_label_select" name="cf_edit_label_select" value="">
+
+	<div class="cf_edit_groups_header">
+		<span><?php _e('Displayed option', 'cforms'); ?></span>
+		<span><?php _e('Optional value', 'cforms'); ?></span>
+	</div>
+	
+	<div id="cf_edit_groups">
+	</div>
+	<div class="add_group_item"><a href="#" id="add_group_button" class="cf_edit_plus"></a></div>
+	
+	<label style="clear:left; padding-top:5px;" for="cf_edit_customerr"><?php _e('Custom error message', 'cforms'); ?></label>
+	<input type="text" id="cf_edit_customerr" name="cf_edit_customerr" value="">
+
+</form>
