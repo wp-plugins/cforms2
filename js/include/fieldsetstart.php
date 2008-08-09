9d90f9330760dfd6d9199542dd4a3c02d4a43821
olivers <olivers@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-08-09 19:09:29 +0000

    git-svn-id: https://plugins.svn.wordpress.org/cforms/tags/8.6.1@58816 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/include/fieldsetstart.php
@@ -0,0 +1,15 @@
+<?php 
+### supporting WP2.6 wp-load
+$abspath='../../../../../';
+if ( file_exists( $abspath . 'wp-load.php') )
+	require_once( $abspath . 'wp-load.php' );
+else
+	require_once( $abspath . 'wp-config.php' );
+?>
+
+<form method="post">
+
+	<label for="cf_edit_label"><?php _e('Name of Fieldset ( only required for <em>New Fieldset</em> )', 'cforms'); ?></label>
+	<input type="text" id="cf_edit_label" name="cf_edit_label" value="">
+
+</form>
